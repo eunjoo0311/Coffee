e86659a9885f3d0c2baa2af420599e81cabcf80a
eunjoo0311 <yyy7423@naver.com>
2023-05-31 13:40:45 +0900

    모달창 완벽 구현!

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
 const modalFront = document.querySelector(".modal-front");
 const modalContainer = document.querySelector(".modal-container");
+const coupon = document.querySelector(".coupon");
+const close = document.getElementById("close");
+const modal = document.getElementById("modal");
 
 modalFront.addEventListener("click", function () {
   modalContainer.classList.toggle("flipped");
+});
+
+coupon.addEventListener("click", function () {
+  modal.style.display = "block";
+});
+
+close.addEventListener("click", function () {
+  modal.style.display = "none";
 });
